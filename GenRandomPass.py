72f0e6350d89a0c43b95c857e11d6925f37b5e1b
Roman <Romazho123@gmail.com>
2020-03-01 13:53:25 -0500

    Make random generation more secure with secrets

--- GenRandomPass.py
+++ GenRandomPass.py
@@ -1,5 +1,6 @@
 import random
 import string
+import secrets
 
 
 def verifyIfInt(stringToPrint):
@@ -31,12 +32,62 @@ def verifyMinMax():
     return [minPassLength, maxPassLength]
 
 
+def genRandomPassword(lengthOfPass):
+    passWord = ""
+    randomInt = random.randint(0, 2)
+    for i in range(lengthOfPass):
+        if(randomInt == 0):
+            passWord += secrets.choice(string.digits)
+            randomInt = random.randint(0, 2)
+        elif(randomInt == 1):
+            passWord += secrets.choice(string.ascii_letters)
+            randomInt = random.randint(0, 2)
+        else:
+            passWord += secrets.choice(string.punctuation)
+            randomInt = random.randint(0, 2)
+
+    return passWord
+
+
+def genRandomCustomPassword(lengthOfPass):
+    # numOfLetters = input("How many letters would you like?")
+    # numOfUpperCaseLetters = input(
+    #     "How many UPPER case letters would you like?")
+    # numOfLowerCaseLetters = input(
+    #     "How many lower case letters would you like?")
+    # numOfNumbers = input("How many numbers would you like?")
+    pass
+
+
+def verifyClientInput(stringToPrint):
+
+    clientAnswer = input(stringToPrint).upper()
+    while (clientAnswer != "N" and clientAnswer != "Y"):
+        print("Invalid input!")
+        clientAnswer = input(stringToPrint).upper()
+        print(clientAnswer)
+
+    return clientAnswer
+
+
 def generatePassword(min, max):
-    lengthOfPass = number = random.randint(min, max)
-    print(lengthOfPass)
+    lengthOfPass = random.randint(min, max)
+
+    isPassFullyCustom = verifyClientInput(
+        "Would you like to fully customize your password? n/y: ")
+
+    if(isPassFullyCustom == "N"):
+        passWord = genRandomPassword(lengthOfPass)
+    else:
+        passWord = genRandomCustomPassword(lengthOfPass)
+
+    print("The length of your password is: " + str(lengthOfPass))
+    print("Password: " + passWord)
 
 
 def main():
+    # characters = string.ascii_letters + string.digits  # + string.punctuation
+
     clientRange = verifyMinMax()
     generatePassword(clientRange[0], clientRange[1])
 
